f04c6f2c6b078e06d9a9dd543bc6070d3c8b507f
Tricia Sawyer <tricia.sawyer03@gmail>
2023-04-24 12:39:40 -0500

    start building out app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
+import React from 'react';
 import './App.css';
+// import axios from 'axios';
 
 
 class App extends React.Component {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {
+      city:''
+
+
+
+    };
+  }
+
+
+
+
+
+
+
+
+
+
 }
+
+
 
 export default App;
