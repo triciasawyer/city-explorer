3f5c368d5caa792fd4137834d1a137fa6192f505
Tricia Sawyer <tricia.sawyer03@gmail>
2023-04-27 07:47:37 -0500

    add weather content

---
+++ src/Weather.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+
+class Weather extends React.Component {
+
+    render() {
+        return (
+            <>
+                <div id='weather-content'>
+                    <h3>Weather:</h3>
+                    {this.props.weather.map((day, index) => (
+                        <WeatherDate key={index} day={day} />
+                    ))}
+
+                </div>
+            </>
+        )
+    }
+
+}
