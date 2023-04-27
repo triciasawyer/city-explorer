b9419df03450cb6d58502b2ea9df79c458ffcd8c
Tricia Sawyer <tricia.sawyer03@gmail>
2023-04-26 19:26:51 -0500

    edit styling

--- src/Coordinates.js
+++ src/Coordinates.js
@@ -8,7 +8,7 @@ class Coordinates extends React.Component {
             <>
                 <Card className="location-cards" id='location'>
                     <Card.Body className='card-body'>
-                        <Card.Title> Welcome to {this.props.city} !</Card.Title>
+                        <Card.Title> Welcome to {this.props.city}!</Card.Title>
                         <Card.Text className='card-text'>Latitude: {this.props.lat}<br></br>Longitude: {this.props.lon}</Card.Text>
                     </Card.Body>
                 </Card>
